0e16f7235ba78ef576986e5226922a6974d26687
brunacostaz <brunacstcandeias@gmail.com>
2024-09-24 22:59:19 -0300

    criação do componente banner

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+const Banner = () => {
+    return (
+        <section className='banner'>
+            <img src='../../imagens/img-header.svg' alt='Imagem da pista de corrida, toda azul e com muitas luzes'></img>
+            <h1>Emoção sem prejudicar o planeta</h1>
+        </section>
+    )
+}
+
+export default Banner;
