b16f43a1cfad753207cec0ca39bdf140b7732e1c
brunacostaz <brunacstcandeias@gmail.com>
2024-09-25 23:32:37 -0300

    alteração da imagem do banner

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -3,8 +3,7 @@ import './Banner.css'
 const Banner = () => {
     return (
         <section className='banner'>
-            <img src='../../imagens/img-header.svg' alt='Imagem da pista de corrida, toda azul e com muitas luzes'></img>
-            <h1>Emoção sem prejudicar o planeta</h1>
+            <img src='../../imagens/img-header.svg' alt='Imagem da pista de corrida, toda azul e com muitas luzes' />
         </section>
     )
 }
