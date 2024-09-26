d0eec428fc6682df5d5a5d64d1aa85fec914c5ca
brunacostaz <brunacstcandeias@gmail.com>
2024-09-26 00:49:42 -0300

    criação do componente para a sessão de explicação sobre a Fórmula E

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Banner from './componentes/Banner';
 import Header from './componentes/Header';
+import SessaoFormulaE from './componentes/SessaoFormulaE';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Banner />
+      <SessaoFormulaE />
     </div>
   );
 }
