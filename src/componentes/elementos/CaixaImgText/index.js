27ea2b141040da7019e20cd3d20fc677c2793dc8
brunacostaz <brunacstcandeias@gmail.com>
2024-09-27 16:57:57 -0300

    criação das caixas de imagem e texto da sessão da solução

---
+++ src/componentes/elementos/CaixaImgText/index.js
@@ -0,0 +1,15 @@
+import './Caixa.css'
+
+const CaixaImgText = (props) => {
+    return (
+        <article
+            className= "caixaImg">
+                <img src={props.imgSrc} alt={props.imgAlt} />
+                <p>
+                    {props.children}
+                </p>
+        </article>
+    )
+}
+
+export default CaixaImgText
