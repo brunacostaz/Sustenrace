3ba561ba4e89491e00ae1bb6561fd724242efe77
brunacostaz <brunacstcandeias@gmail.com>
2024-09-27 13:31:30 -0300

    adição de um hover para a borda

--- src/componentes/SessaoSustenrace/index.js
+++ src/componentes/SessaoSustenrace/index.js
@@ -1,10 +1,13 @@
 import './Sustenrace.css'
 
 const SessaoSustenrace = () => {
+
     return (
         <section className='containerSustenrace'>
             <h1>E onde entra a Sustenrace nessa história?</h1>
-            <article className='caixaObjetivo'>
+            <article 
+                className= 'caixaObjetivo'
+                >
                 <img src='../../../imagens/logo-sustenrace.png'/>
                 <p>
                     O nosso objetivo é proporcionar <strong>experiências inovadoras</strong> e envolventes por meio da interatividade com as corridas.
