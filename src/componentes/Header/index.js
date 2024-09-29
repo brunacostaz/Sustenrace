4212d3a4582696ad8d712c2a6f5aa243ac0d7be4
brunacostaz <brunacstcandeias@gmail.com>
2024-09-29 20:45:57 -0300

    remoção do navbar e junção de todo o código no header

--- src/componentes/Header/index.js
+++ src/componentes/Header/index.js
@@ -1,4 +1,3 @@
-import NavBar from '../Navbar';
 import './Header.css'
 import '../../variaveis.css'
 
@@ -11,7 +10,17 @@ const Header = () => {
                 </a>
                 <span>Sustenrace</span>
             </div>
-            <NavBar />
+            <nav className='navbar'>
+                <ul className='menuLinks'>
+                        <li><a href="#formulaE">Fórmula E</a></li>
+                        <li><a href="#impactos">Impactos da Corrida</a></li>
+                        <li><a href="#sustenrace">Sustenrace</a></li>
+                        <li><a href="#produtos">Produtos</a></li>
+                        <li><a href="#carro">Carro 3D</a></li>
+                        <li><a href="#bpm">Batimentos Cardíacos</a></li>
+                        <li><a href="#forum">Fórum</a></li>
+                </ul>
+        </nav>
         </header>
     )
 }
