689865b89e77026349183d6dfbf71ace1410fdf4
brunacostaz <brunacstcandeias@gmail.com>
2024-09-30 10:01:39 -0300

    ajuste do caminho do banner

--- src/componentes/elementos/Banner/index.js
+++ src/componentes/elementos/Banner/index.js
@@ -3,7 +3,7 @@ import './Banner.css'
 const Banner = () => {
     return (
         <section className='banner'>
-            <img src='../../imagens/img-header.svg' alt='Imagem da pista de corrida, toda azul e com muitas luzes' />
+            <img src='../../../../public/imagens/img-header.svg' alt='Imagem da pista de corrida, toda azul e com muitas luzes' />
         </section>
     )
 }
