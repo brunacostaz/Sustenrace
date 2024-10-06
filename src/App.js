670043a7dd3ff5826cc49f1fc4b090d71816b205
brunacostaz <brunacstcandeias@gmail.com>
2024-10-06 01:24:37 -0300

    removendo as rotas que serão criadas, inicialmente, em outra branch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,53 @@
+import Banner from './componentes/elementos/Banner';
 import Footer from './componentes/Footer';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './componentes/Home'
-import Carros3D from './componentes/Carros3D';
+import Header from './componentes/Header';
+import AbaForum from './componentes/SessoesHome/AbaForum';
+import SessaoBPM from './componentes/SessoesHome/SessaoBPM';
+import SessaoFormulaE from './componentes/SessoesHome/SessaoFormulaE';
+import SessaoImpactos from './componentes/SessoesHome/SessaoImpactos';
+import SessaoRecompensas from './componentes/SessoesHome/SessaoRecompensas';
+import SessaoSolucao from './componentes/SessoesHome/SessaoSolucao';
+import SessaoSustenrace from './componentes/SessoesHome/SessaoSustenrace';
+import SessaoUniao from './componentes/SessoesHome/SessaoUniao';
+import CarroHome from './componentes/SessoesHome/CarroHome';
 
 function App() {
-  return (
-    <Router basename='/Sustenrace'>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path='/carros3D' element={<Carros3D />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <div className="App">
+            <section id='home'>
+                <Header />
+            </section>
+            <Banner />
+            <section id='formulaE'>
+                <SessaoFormulaE />
+            </section>
+            <section id='impactos'>
+                <SessaoImpactos />
+            </section>
+            <section id='sustenrace'>
+                <SessaoSustenrace />
+            </section>
+            <section>
+                <SessaoUniao />
+            </section>
+            <section id='produtos'>
+                <SessaoSolucao />
+            </section>
+            <section>
+                <SessaoRecompensas />
+            </section>
+            <section id='carro'>
+                <CarroHome />
+            </section>
+            <section id="bpm">
+                <SessaoBPM />
+            </section>
+            <section id='forum'>
+                <AbaForum />
+            </section>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
